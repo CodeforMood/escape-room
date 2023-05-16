939b49b05c566e13db5acb293b3439ed5709b5fe
Vladislav <121075773+Rocket151@users.noreply.github.com>
2023-05-16 11:49:55 +0000

    Update 5 files

--- src/pages/booking-screen/booking-screen.tsx
+++ src/pages/booking-screen/booking-screen.tsx
@@ -1,32 +1,7 @@
 export default function BookingScreen (): JSX.Element {
   return (
     <div className="wrapper">
-      <header className="header">
-        <div className="container container--size-l">
-          <a className="logo header__logo" href="index.html" aria-label="Перейти на Главную">
-            <svg width="134" height="52" aria-hidden="true">
-              <use xlinkHref="#logo"></use>
-            </svg>
-          </a>
-          <nav className="main-nav header__main-nav">
-            <ul className="main-nav__list">
-              <li className="main-nav__item">
-                <a className="link not-disabled active" href="index.html">Квесты</a>
-              </li>
-              <li className="main-nav__item">
-                <a className="link" href="contacts.html">Контакты</a>
-              </li>
-              <li className="main-nav__item">
-                <a className="link" href="my-quests.html">Мои бронирования</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="header__side-nav">
-            <a className="btn btn--accent header__side-item" href="#">Выйти</a>
-            <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="page-content decorated-page">
         <div className="decorated-page__decor" aria-hidden="true">
           <picture>
@@ -59,33 +34,33 @@ export default function BookingScreen (): JSX.Element {
                   <label className="custom-radio booking-form__date">
                     <input type="radio" id="today15h00m" name="date" checked required value="today15h00m" /><span className="custom-radio__label">15:00</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="today17h30m" name="date" required value="today17h30m" /><span className="custom-radio__label">17:30</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="today19h30m" name="date" required value="today19h30m" disabled /><span className="custom-radio__label">19:30</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="today21h30m" name="date" required value="today21h30m" /><span className="custom-radio__label">21:30</span>
                   </label>
                 </div>
               </fieldset>
-              <fieldset class="booking-form__date-section">
-                <legend class="booking-form__date-title">Завтра</legend>
-                <div class="booking-form__date-inner-wrapper">
-                  <label class="custom-radio booking-form__date">
+              <fieldset className="booking-form__date-section">
+                <legend className="booking-form__date-title">Завтра</legend>
+                <div className="booking-form__date-inner-wrapper">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="tomorrow11h00m" name="date" required value="tomorrow11h00m" /><span className="custom-radio__label">11:00</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="tomorrow15h00m" name="date" required value="tomorrow15h00m" disabled /><span className="custom-radio__label">15:00</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="tomorrow17h30m" name="date" required value="tomorrow17h30m" disabled /><span className="custom-radio__label">17:30</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="tomorrow19h45m" name="date" required value="tomorrow19h45m" /><span className="custom-radio__label">19:45</span>
                   </label>
-                  <label class="custom-radio booking-form__date">
+                  <label className="custom-radio booking-form__date">
                     <input type="radio" id="tomorrow21h30m" name="date" required value="tomorrow21h30m" /><span className="custom-radio__label">21:30</span>
                   </label>
                 </div>
@@ -94,36 +69,38 @@ export default function BookingScreen (): JSX.Element {
             <fieldset className="booking-form__section">
               <legend className="visually-hidden">Контактная информация</legend>
               <div className="custom-input booking-form__input">
-                <label className="custom-input__label" for="name">Ваше имя</label>
+                <label className="custom-input__label" htmlFor="name">Ваше имя</label>
                 <input type="text" id="name" name="name" placeholder="Имя" required pattern="[А-Яа-яЁёA-Za-z'- ]{1,}" />
               </div>
               <div className="custom-input booking-form__input">
-                <label className="custom-input__label" for="tel">Контактный телефон</label>
+                <label className="custom-input__label" htmlFor="tel">Контактный телефон</label>
                 <input type="tel" id="tel" name="tel" placeholder="Телефон" required pattern="[0-9]{10,}" />
               </div>
               <div className="custom-input booking-form__input">
-                <label className="custom-input__label" for="person">Количество участников</label>
+                <label className="custom-input__label" htmlFor="person">Количество участников</label>
                 <input type="number" id="person" name="person" placeholder="Количество участников" required />
               </div>
               <label className="custom-checkbox booking-form__checkbox booking-form__checkbox--children">
-                <input type="checkbox" id="children" name="children" checked><span className="custom-checkbox__icon" />
+                <input type="checkbox" id="children" name="children" checked />
+                <span className="custom-checkbox__icon">
                   <svg width="20" height="17" aria-hidden="true">
                     <use xlinkHref="#icon-tick"></use>
-                  </svg></span><span className="custom-checkbox__label">Со&nbsp;мной будут дети</span>
+                  </svg>
+                </span>
+                <span className="custom-checkbox__label">Со&nbsp;мной будут дети</span>
               </label>
             </fieldset>
             <button className="btn btn--accent btn--cta booking-form__submit" type="submit">Забронировать</button>
             <label className="custom-checkbox booking-form__checkbox booking-form__checkbox--agreement">
-              <input type="checkbox" id="id-order-agreement" name="user-agreement" required><span className="custom-checkbox__icon" />
+              <input type="checkbox" id="id-order-agreement" name="user-agreement" required /><span class="custom-checkbox__icon">
                 <svg width="20" height="17" aria-hidden="true">
-                  <use xlinkHref="#icon-tick"></use>
+                  <use xlinHref="#icon-tick"></use>
                 </svg></span><span className="custom-checkbox__label">Я&nbsp;согласен с
-                <a class="link link--active-silver link--underlined" href="#">правилами обработки персональных данных</a>&nbsp;и пользовательским соглашением</span>
+                <a className="link link--active-silver link--underlined" href="#">правилами обработки персональных данных</a>&nbsp;и пользовательским соглашением</span>
             </label>
           </form>
         </div>
       </main>
-      <Footer />
-    </div>
+    <div>
   );
 }
